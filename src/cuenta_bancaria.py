2f697c3047a886538733634a8da77a1865d6c192
user <user@email.com>
2024-09-07 14:14:17 -0500

    solucion

--- src/cuenta_bancaria.py
+++ src/cuenta_bancaria.py
@@ -1,2 +1,33 @@
 class CuentaBancaria:
-    # TODO: Aquí va el código de la clase CuentaBancaria
+    def __init__(self, titular, numero_cuenta, saldo):
+        self._titular = titular
+        self._numero_cuenta = numero_cuenta
+        self._saldo = saldo
+        self._tipo_interes = 0  # Tipo de interés en porcentaje
+
+    def get_titular(self):
+        return self._titular
+
+    def set_titular(self, titular):
+        self._titular = titular
+
+    def get_numero_cuenta(self):
+        return self._numero_cuenta
+
+    def get_saldo(self):
+        return self._saldo
+
+    def ingresar(self, cantidad):
+        if cantidad > 0:
+            self._saldo += cantidad
+
+    def retirar(self, cantidad):
+        if 0 < cantidad <= self._saldo:
+            self._saldo -= cantidad
+
+    def set_tipo_interes(self, tipo_interes):
+        if tipo_interes >= 0:
+            self._tipo_interes = tipo_interes
+
+    def calcular_interes(self):
+        return self._saldo * self._tipo_interes / 100
--- src/principal.py
+++ src/principal.py
@@ -1,2 +1,39 @@
+from cuenta_bancaria import CuentaBancaria
+
 if __name__ == '__main__':
-    # TODO: Adiciona aquí el código que deseas para la Cuenta Bancaria.
+    cuenta = CuentaBancaria("Wendy Fonseca", "5072317169", 200000)
+
+    assert cuenta.get_titular() == "Wendy Fonseca"
+
+    cuenta.set_titular("Karen Tacuma")
+    assert cuenta.get_titular() == "Karen Tacuma"
+
+    assert cuenta.get_numero_cuenta() == "5072317169"
+
+    assert cuenta.get_saldo() == 200000
+
+    cuenta.ingresar(50000)
+    assert cuenta.get_saldo() == 250000
+
+    cuenta.ingresar(20000)
+    assert cuenta.get_saldo() == 270000
+
+    cuenta.retirar(80000)
+    assert cuenta.get_saldo() == 190000
+
+    cuenta.retirar(150000)
+    assert cuenta.get_saldo() == 40000
+
+    cuenta.set_tipo_interes(0)
+    assert cuenta.calcular_interes() == 0
+
+    cuenta.set_tipo_interes(5)
+    assert cuenta.calcular_interes() == 2000  # 5% de 40000
+
+    cuenta.set_tipo_interes(2)
+    assert cuenta.calcular_interes() == 800  # 2% de 400000
+
+    cuenta.set_tipo_interes(12)
+    assert cuenta.calcular_interes() == 4800  # 12% de 40000
+
+    print("Todas las pruebas fueron correctas")
